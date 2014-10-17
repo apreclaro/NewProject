d00a21f867eec845e9be974b0a578eadfe12e24a
apreclaro <alfredo.preclaro@gmail.com>
2014-10-16 18:59:11 -0700

    Create class1.cs

---
+++ class1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C2Lab1
+{
+    public class Class1
+    {
+        public string fname;
+        public string lname;
+        public string major;
+        public string gpa;
+
+    }
+}
